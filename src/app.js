6cdc04f02a4490d3a67f445934c26b7dbb86cf17
CMF21 <cristianmauriciofalcon@gmail.com>
2024-02-22 23:16:12 -0300

    subiendo actividad1, creando ruta y archivo login y register y subiendo imgs

--- src/app.js
+++ src/app.js
@@ -6,17 +6,23 @@ const app = express();
 const path = require('path');
 
 // creando las rutas
+// ruta raiz
 app.get('/',(req,res)=>{
-    //           linkeo la ruta abs de home.html
+    //-----------linkeo la ruta abs de home.html
     res.sendFile(path.resolve(__dirname,'./views/home.html'))
 })
-
+// ruta de login
+app.get('/login',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./views/login.html'))
+})
+// ruta de register
+app.get('/register',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./views/register.html'))
+})
 
 //seteando los archivos estaticos de la carpeta public
 const publicPath = path.resolve(__dirname,'../public');
 app.use(express.static(publicPath));
-
-
 
 //levantando el servidor
 app.listen(3001,(req,res)=>{
